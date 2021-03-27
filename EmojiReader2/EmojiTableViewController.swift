f201eefd1a168b8407213ef1f44019305748ef34
devGod <devgod@mail.ru>
2021-03-27 17:29:52 +0300

    add title "Emoji Reader"

--- EmojiReader2/EmojiTableViewController.swift
+++ EmojiReader2/EmojiTableViewController.swift
@@ -16,6 +16,7 @@ class EmojiTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.title = "Emoji Reader"
          self.navigationItem.leftBarButtonItem = self.editButtonItem
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
