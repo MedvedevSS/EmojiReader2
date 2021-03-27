c725f4ff3de21de8f5760bfddcc14806aa9ec5bd
devGod <devgod@mail.ru>
2021-03-27 17:59:14 +0300

    add Hor Stack with label and add Ver Stack with two labels

---
+++ EmojiReader2/EmojiTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader2
+//
+//  Created by Сергей Медведев on 27.03.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+
+}
