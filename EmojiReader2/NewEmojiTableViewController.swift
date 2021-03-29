11b7c214a6d5c7ebdd2d798e23283814fc33bcf9
devGod <devgod@mail.ru>
2021-03-29 20:32:52 +0300

    add new cells Function

--- EmojiReader2/NewEmojiTableViewController.swift
+++ EmojiReader2/NewEmojiTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
     
+    var emoji = Emoji(emoji: "", name: "", description: "", isFovourite: false)
+    
     @IBOutlet weak var emojiTextfield: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextfield: UITextField!
@@ -19,8 +21,28 @@ class NewEmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        updateSabeButtonState()
     }
+    
+    private func updateSabeButtonState() {
+        let emojiText = emojiTextfield.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextfield.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
     @IBAction func testChanged(_ sender: UITextField) {
+        updateSabeButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTextfield.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextfield.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFovourite: self.emoji.isFovourite)
     }
 }
