18095c03af0dc547cb96547c805e96d438f0c50c
devGod <devgod@mail.ru>
2021-03-29 11:02:23 +0300

    commit

--- EmojiReader2/EmojiTableViewController.swift
+++ EmojiReader2/EmojiTableViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class EmojiTableViewController: UITableViewController {
-
+    
+    let objects = [
+        Emoji(emoji: "🥰", name: "Love", description: "Let's love each other", isFovourite: false),
+        Emoji(emoji: "⚽️", name: "Football", description: "Let's play football together", isFovourite: false),
+        Emoji(emoji: "🐱", name: "Cat", description: "Cat is the cutest animal", isFovourite: false)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,13 +36,14 @@ class EmojiTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return objects.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
-        cell.emojiLabel.text = "😎"
+        let object = objects[indexPath.row]
+        cell.set(object: object)
 
         return cell
     }
